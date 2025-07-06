ab0eb1725ac4f94ba39e1325cfccab359fef0abd
WilliamT1109 <william.thibault1109@gmail.com>
2025-07-06 13:45:26 -0400

    Grid progress

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
 import "./about.css"
 
 const About = () => {
-  return ( 
-    <div></div>
-    )
-}
+  return (
+    <div className="grid-container">
+      <div className="item left-1">Left 1</div>
+      <div className="item left-2">Left 2</div>
+      <div className="item right-1">
+        <div className='icon'>
+          <i></i>
+        </div>
+        <h3>Information Technology</h3>
+      </div>
+      <div className="item right-2">Right 2</div>
+      <div className="item right-3">Right 3</div>
+    </div>
+  );
+};
 
 export default About
